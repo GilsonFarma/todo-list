d0928e30833435ce334c2c3d9e57b44266498859
gilson <m0t1v0l007@gmail.com>
2026-03-31 21:53:49 -0300

    feat: adicionar tarefas com localStorage e remoção

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("input-tarefa");
+const btn = document.getElementById("btn-adicionar")
+const lista = document.getElementById("lista-tarefas")
+
+let tarefas = dados || [];
+const dados = JSON.parse(localStorage.getItem('tarefas'));
+
+function adicionarTarefa() {
+    const texto = input.value;
+    if(!texto) return;
+    const novoItem = { id: Date.now(), nome: texto }
+    tarefas.push(novoItem);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    const item = document.createElement('li');
+    item.textContent = texto;
+    lista.appendChild(item);
+    input.value = ""
+
+    const remove = document.createElement('button');
+    remove.textContent = "x"
+    remove.addEventListener('click', () => {
+        item.remove()
+    })
+    item.appendChild(remove)
+}
+
+btn.addEventListener('click', adicionarTarefa)
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        adicionarTarefa()
+    }
+})
+
